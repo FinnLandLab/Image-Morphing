a6622ed0428a5ea73daf6ed9127778cb80d6387b
Henrique Matulis <henrique.matulis@mail.utoronto.ca>
2018-01-27 15:52:36 -0500

    Add: print statements to show progress

--- script.py
+++ script.py
@@ -1,15 +1,34 @@
 from PIL import Image
 import numpy as np
 import Polygon
+import os
 
-data1 = np.array(Image.open('test1.png'))
-data2 = np.array(Image.open('test2.png'))
+image1_path = 'shape1.png'
+image2_path = 'shape2.png'
 
-edge1_pxs = Polygon.get_edge_polygon(data1)
-edge2_pxs = Polygon.get_edge_polygon(data2)
+morph_quality = 20
 
-edge1_pxs.align(edge2_pxs)
+directory = "out/{}-{}/".format(image1_path[:image1_path.index('.')], image2_path[:image2_path.index('.')])
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-for i, c in enumerate(np.linspace(0, 1, 20)):
-    interpol = edge1_pxs.interpol(edge2_pxs, c)
-    Image.fromarray(interpol.get_image(data1.shape)).save('out/{i:03d}_interpol_{c:}.png'.format(i=i, c=c))
+print("Loading images...")
+data1 = np.array(Image.open(image1_path))
+print('Loaded {}'.format(image1_path))
+data2 = np.array(Image.open(image2_path))
+print('Loaded {}'.format(image2_path))
+
+print("Converting images to polygons...")
+polygon1 = Polygon.get_edge_polygon(data1)
+print("Converted {} to a polygon with {} vertexes".format(image1_path, len(polygon1.pixels)))
+polygon2 = Polygon.get_edge_polygon(data2)
+print("Converted {} to a polygon with {} vertexes".format(image2_path, len(polygon2.pixels)))
+
+print("Aligning the polygons...")
+polygon1.align(polygon2)
+
+print("Saving the morphed images...")
+for i, c in enumerate(np.linspace(0, 1, morph_quality)):
+    name = '{i:03d}_interpol.png'.format(i=i)
+    interpol = polygon1.interpol(polygon2, c)
+    Image.fromarray(interpol.get_image(data1.shape)).save(directory + name)
